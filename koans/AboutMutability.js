899ff10e31adc85e64bf4a026a8523ab06277cc2
Adrienne Lim <adriennelim.mc@gmail.com>
2019-01-11 17:46:53 -0800

    update koans, pair-programming with Hector Williams

--- koans/AboutMutability.js
+++ koans/AboutMutability.js
@@ -1,10 +1,14 @@
 describe("About Mutability", function() {
 
   it("should expect object properties to be public and mutable", function () {
-    var aPerson = {firstname: "John", lastname: "Smith" };
+    var aPerson = { 
+      firstname: "John", 
+      lastname: "Smith" 
+    };
+    
     aPerson.firstname = "Alan";
     
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+    expect(aPerson.firstname).toBe("Alan");
   });
 
   it("should understand that constructed properties are public and mutable", function () {
@@ -16,7 +20,7 @@ describe("About Mutability", function() {
     var aPerson = new Person ("John", "Smith");
     aPerson.firstname = "Alan";
     
-    expect(aPerson.firstname).toBe(FILL_ME_IN);
+    expect(aPerson.firstname).toBe("Alan");
   });
 
   it("should expect prototype properties to be public and mutable", function () {
@@ -25,21 +29,26 @@ describe("About Mutability", function() {
       this.firstname = firstname;
       this.lastname = lastname;
     }
+
     Person.prototype.getFullName = function () {
       return this.firstname + " " + this.lastname;
     };
     
-    var aPerson = new Person ("John", "Smith");
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    var aPerson = new Person ("John", "Smith");   // new obj ref
+    
+    expect(aPerson.getFullName()).toBe("John Smith");
     
     aPerson.getFullName = function () {
       return this.lastname + ", " + this.firstname;
     };
     
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe("Smith, John");
   });
+    
+
 
   it("should know that variables inside a constructor and constructor args are private", function () {
+    
     function Person(firstname, lastname)
     {
       var fullName = firstname + " " + lastname;
@@ -48,21 +57,23 @@ describe("About Mutability", function() {
       this.getLastName = function () { return lastname; };
       this.getFullName = function () { return fullName; };
     }
+
     var aPerson = new Person ("John", "Smith");
 
-    aPerson.firstname = "Penny";
-    aPerson.lastname = "Andrews";
-    aPerson.fullName = "Penny Andrews";
+    aPerson.firstname = "Penny";   //     new key/value pair 
+    aPerson.lastname = "Andrews";   //     ""       " "
     
-    expect(aPerson.getFirstName()).toBe(FILL_ME_IN);
-    expect(aPerson.getLastName()).toBe(FILL_ME_IN);
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    aPerson.fullName = "Penny Andrews";  // no change 
+    
+    expect(aPerson.getFirstName()).toBe("John");
+    expect(aPerson.getLastName()).toBe("Smith");
+    expect(aPerson.getFullName()).toBe("John Smith");
 
-    aPerson.getFullName = function () {
-      return aPerson.lastname + ", " + aPerson.firstname;
+    aPerson.getFullName = function () {     // overwrite method 
+      return aPerson.lastname + ", " + aPerson.firstname;   //using new key/value pairs 
     };
     
-    expect(aPerson.getFullName()).toBe(FILL_ME_IN);
+    expect(aPerson.getFullName()).toBe("Andrews, Penny");
   });
 
 });
